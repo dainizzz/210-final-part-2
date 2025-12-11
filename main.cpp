cead9b5367f7bc11983c56df37f5fd56c163ddb7
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2025-12-10 22:59:39 -0800

    10 minute update

--- main.cpp
+++ main.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-	std::cout << "Hello, World!" << std::endl;
+	/* MILESTONE 1 (Coffee Booth pt 1)
+	 * Create a node struct for a linked list with string name and string drink order as the data in each node
+	 * Use LLM for creating data set of data arrays w/ names and drink orders
+	 */
+
+	/* MILESTONE 2 (Coffee Booth pt 2)
+	 * Run simulation for 10 rounds and initialize queue w/ 3 customers
+	 * In any round:
+	 * 50% chance someone joins queue
+	 * Customer at the head of the queue is ALWAYS served in each round
+	 * If the queue is empty:
+	 * no customer is served, but 50% chance someone joins still remains
+	 */
+
+	/* MILESTONE 3 (Muffin Booth)
+	 * Add code simulating the queue of a muffin booth using a deque, and incorporate this new booth into the
+	 * simulation while keeping the probabilities the same
+	 */
+
+	/* MILESTONE 4 (Friendship Bracelets)
+	 * Add code simulating a friendship bracelet vendor using a vector
+	 */
+
+	/* MILESTONE 5
+	 * Add another vendor showing use of a different data structure not yet used in this program selling something
+	 * you choose
+	 */
+
+	/* MILESTONE 6
+	 * Make sure each round shows all 4 booths' activities
+	 */
+
 	return 0;
 }
